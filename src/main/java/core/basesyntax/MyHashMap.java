93d48990a3cf83ef33e7b9f74577b01bd48e6755
Maks <maksimronkanen940@gmail.com>
2025-10-04 00:22:49 +0300

    Solution

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,108 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int DEFAULT_CAPACITY = 16;
+    private static final float DEFAULT_LOAD_FACTOR = 0.75f;
+    private static final int RESIZE_MULTIPLIER = 2;
+    private Node<K, V>[] buckets;
+    private int size = 0;
+    private int capacity;
+    private double loadFactor;
+
+    public MyHashMap() {
+        buckets = new Node[DEFAULT_CAPACITY];
+        size = 0;
+        capacity = DEFAULT_CAPACITY;
+        loadFactor = DEFAULT_LOAD_FACTOR;
+    }
+
+    public int getBucketIndex(K key) {
+        return key == null ? 0 : Math.abs(key.hashCode() % buckets.length);
+    }
+
+    public void resize() {
+        int newCapacity = capacity * RESIZE_MULTIPLIER;;
+        Node<K, V>[] newBuckets = new Node[newCapacity];
+
+        for (Node<K, V> head : buckets) {
+            Node<K, V> current = head;
+            while (current != null) {
+                Node<K, V> next = current.next;
+                int newIndex = current.key == null
+                        ? 0 : Math.abs(current.key.hashCode() % newCapacity);
+
+                current.next = newBuckets[newIndex];
+                newBuckets[newIndex] = current;
+
+                current = next;
+            }
+        }
+        buckets = newBuckets;
+        capacity = newCapacity;
+    }
 
     @Override
     public void put(K key, V value) {
+        int index = getBucketIndex(key);
 
+        if (buckets[index] == null) {
+            buckets[index] = new Node<>(key, value, null);
+            size++;
+            if (size >= capacity * loadFactor) {
+                resize();
+            }
+            return;
+        }
+
+        Node<K, V> current = buckets[index];
+        while (true) {
+            if (Objects.equals(current.key, key)) {
+                current.value = value;
+                return;
+            }
+
+            if (current.next == null) {
+                current.next = new Node<>(key, value, null);
+                size++;
+                if (size >= capacity * loadFactor) {
+                    resize();
+                }
+                return;
+            }
+
+            current = current.next;
+        }
     }
 
     @Override
     public V getValue(K key) {
+        int index = getBucketIndex(key);
+
+        Node<K, V> current = buckets[index];
+        while (current != null) {
+            if (Objects.equals(current.key, key)) {
+                return current.value;
+            }
+            current = current.next;
+        }
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    private static class Node<K, V> {
+        private V value;
+        private final K key;
+        private Node<K, V> next;
+
+        Node(K key, V value, Node<K, V> next) {
+            this.key = key;
+            this.value = value;
+        }
     }
 }
